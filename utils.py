24eb997f2d37be8a320891485228adeb35385519
Kostiantyn27 <kostia.fedoroff1@gmail.com>
2021-01-07 19:34:50 +0200

    fix

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import string
 import random
 import math
 import datetime
+import time
 
 
 def generate_password(length: int) -> str:
@@ -11,7 +12,7 @@ def generate_password(length: int) -> str:
     :return: Random symbols
     """
     if not isinstance(length, int):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     letters = string.ascii_letters
     password = ""
@@ -21,12 +22,7 @@ def generate_password(length: int) -> str:
     return password
 
 
-if __name__ == '__main__':
-    assert len(generate_password(5)) == 5
-    assert len(generate_password(10)) == 10
-    assert len(generate_password(24)) == 24
-    assert len(generate_password(5)) != 4
-    assert type(generate_password(5)) == str
+key = 2
 
 
 def encrypt_message(message: str) -> str:
@@ -36,22 +32,12 @@ def encrypt_message(message: str) -> str:
     :return: Encrypts string
     """
     if not isinstance(message, str):
-        return "Wrong data type"
-
-    key = 2
+        raise TypeError("Wrong data type")
 
     return ''.join(
         chr(num + key)
         for num in map(ord, message)
     )
-
-
-if __name__ == '__main__':
-    assert encrypt_message('Dima') == 'Fkoc'
-    assert encrypt_message('Kostia') == 'Mquvkc'
-    assert encrypt_message('Bugsnax') == 'Dwiupcz'
-    assert encrypt_message('Hello World!') == 'Jgnnq"Yqtnf#'
-    assert type(encrypt_message('Bugsnax')) == str
 
 
 def lucky_number(ticket: str) -> bool:
@@ -62,7 +48,7 @@ def lucky_number(ticket: str) -> bool:
     :return: Boolian
     """
     if not isinstance(ticket, str) or len(ticket) != 6:
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     sum_one = 0
     sum_second = 0
@@ -73,15 +59,7 @@ def lucky_number(ticket: str) -> bool:
     for num in ticket[3:]:
         sum_second += int(num)
 
-    return True if sum_one == sum_second else False
-
-
-if __name__ == '__main__':
-    assert lucky_number("667766") == True
-    assert lucky_number("111111") == True
-    assert lucky_number("000000") == True
-    assert lucky_number("123456") == False
-    assert type(lucky_number("123456")) == bool
+    return sum_one == sum_second
 
 
 def fizz_buzz(num: int) -> str:
@@ -95,7 +73,7 @@ def fizz_buzz(num: int) -> str:
     :return: String
     """
     if not isinstance(num, int):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     if not num % 3 and not num % 5:
         return "FizzBuzz"
@@ -105,14 +83,6 @@ def fizz_buzz(num: int) -> str:
         return "Fizz"
     else:
         return str(num)
-
-
-if __name__ == '__main__':
-    assert fizz_buzz(15) == "FizzBuzz"
-    assert fizz_buzz(30) == "FizzBuzz"
-    assert fizz_buzz(18) == "Fizz"
-    assert fizz_buzz(25) == "Buzz"
-    assert fizz_buzz(13) == "13"
 
 
 def password_is_strong(password: str) -> bool:
@@ -126,7 +96,7 @@ def password_is_strong(password: str) -> bool:
     :return: Boolian
     """
     if not isinstance(password, str):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     if len(password) < 10 \
             or password.isdigit() \
@@ -140,14 +110,6 @@ def password_is_strong(password: str) -> bool:
         return False
 
 
-if __name__ == '__main__':
-    assert password_is_strong("12345678Da") == True
-    assert password_is_strong("1234567890") == False
-    assert password_is_strong("12345Da") == False
-    assert password_is_strong("&&&&&&&&&&") == False
-    assert password_is_strong("yfugwuy32674896>V>") == False
-
-
 def number_is_prime(num: int) -> bool:
     """ This function determines if the number is prime
 
@@ -155,7 +117,7 @@ def number_is_prime(num: int) -> bool:
     :return: Boolian
     """
     if not isinstance(num, int):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     if num == 1:
         return False
@@ -169,14 +131,6 @@ def number_is_prime(num: int) -> bool:
     return True
 
 
-if __name__ == '__main__':
-    assert number_is_prime(2) == True
-    assert number_is_prime(1) == False
-    assert number_is_prime(45) == False
-    assert number_is_prime(67) == True
-    assert number_is_prime(75) == False
-
-
 def decrypt_message(message: str) -> str:
     """ This function decrypts the message by key
 
@@ -186,20 +140,10 @@ def decrypt_message(message: str) -> str:
     if not isinstance(message, str):
         return "Wrong data type"
 
-    key = 2
-
     return ''.join(
         chr(num - key)
         for num in map(ord, message)
     )
-
-
-if __name__ == '__main__':
-    assert decrypt_message('Fkoc') == 'Dima'
-    assert decrypt_message('Mquvkc') == 'Kostia'
-    assert decrypt_message('Dwiupcz') == 'Bugsnax'
-    assert decrypt_message('Jgnnq"Yqtnf#') == 'Hello World!'
-    assert type(decrypt_message('WDiuhf')) == str
 
 
 def volume_of_sphere(radius: float) -> float:
@@ -209,23 +153,11 @@ def volume_of_sphere(radius: float) -> float:
     :return: Sphere Volume
     """
     if not isinstance(radius, float):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     return float('%.2f' % ((4/3)*math.pi*math.pow(radius, 3)))
 
 
-if __name__ == '__main__':
-    assert volume_of_sphere(10.0) == 4188.79
-    assert volume_of_sphere(10.0) != 4188.7902
-    assert volume_of_sphere(10) == "Wrong data type"
-    assert volume_of_sphere(5.5) == 696.91
-    assert volume_of_sphere(20.0) == 33510.32
-
-
-# Не совсем понимаю зачем эта функция.
-# Можно сразу закинуть в параметры days_diff(datetime.date(2020, 1, 5), datetime.date(2020, 1, 1)
-# И код вышел бы в 2 строчки
-# Но удобнее вводить так, но может кому-то удобнее по-другому.
 def days_diff(start_date: ..., end_date: ...) -> int:
     """ The function calculates the difference in days between dates
 
@@ -234,27 +166,16 @@ def days_diff(start_date: ..., end_date: ...) -> int:
     :return: integer (days)
     """
     if not isinstance(start_date, str) and not isinstance(end_date, str):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
-    start_date = start_date.split("-")
-    end_date = end_date.split("-")
+    start_date = time.strptime(start_date,"%Y-%m-%d")
+    end_date = time.strptime(end_date,"%Y-%m-%d")
 
-    start_year, start_month, start_day = start_date
-    end_year, end_month, end_day = end_date
-
-    start_date = datetime.date(int(start_year), int(start_month), int(start_day))
-    end_date = datetime.date(int(end_year), int(end_month), int(end_day))
+    start_date = datetime.date(int(start_date[0]), int(start_date[1]), int(start_date[2]))
+    end_date = datetime.date(int(end_date[0]), int(end_date[1]), int(end_date[2]))
 
     result = start_date - end_date
     return int(math.fabs(result.days))
-
-
-if __name__ == '__main__':
-    assert days_diff("2021-1-5", "2021-1-1") == 4
-    assert days_diff("2021-01-05", "2021-01-01") == 4
-    assert days_diff("2021-1-5", "2020-1-5") == 366
-    assert days_diff("2020-1-5", "2021-1-5") == 366
-    assert days_diff("2021-1-10", "2021-1-10") == 0
 
 
 def prs(client_choice: str) -> bool:
@@ -265,10 +186,10 @@ def prs(client_choice: str) -> bool:
     :return: True or False depending on the condition ... > rock > scissors > paper > rock > scissors > ...
     """
     if not isinstance(client_choice, str):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
-    if client_choice != "paper" and client_choice != "rock" and client_choice != "scissors":
-        return "Wrong atribute"
+    if client_choice not in ("paper", "rock", "scissors"):
+        raise TypeError("Wrong atribute")
 
     enemy_choice = random.choice(["paper", "rock", "scissors"])
 
@@ -284,14 +205,6 @@ def prs(client_choice: str) -> bool:
     return True
 
 
-if __name__ == '__main__':
-    assert prs(12) == "Wrong data type"
-    assert prs("papers") == "Wrong atribute"
-    assert type(prs("paper")) == bool
-    assert type(prs("scissors")) == bool
-    assert type(prs("rock")) == bool
-
-
 def integer_as_roman(integer: int) -> str:
     """ This function converts Arabic numbers to Roman numbers in the range 1 to 3999
 
@@ -299,7 +212,7 @@ def integer_as_roman(integer: int) -> str:
     :return: roman number
     """
     if not isinstance(integer, int):
-        return "Wrong data type"
+        raise TypeError("Wrong data type")
 
     roman = ""
     roman_numbers_dict = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
@@ -346,6 +259,64 @@ def integer_as_roman(integer: int) -> str:
 
 
 if __name__ == '__main__':
+    assert len(generate_password(5)) == 5
+    assert len(generate_password(10)) == 10
+    assert len(generate_password(24)) == 24
+    assert len(generate_password(5)) != 4
+    assert type(generate_password(5)) == str
+
+    assert encrypt_message('Dima') == 'Fkoc'
+    assert encrypt_message('Kostia') == 'Mquvkc'
+    assert encrypt_message('Bugsnax') == 'Dwiupcz'
+    assert encrypt_message('Hello World!') == 'Jgnnq"Yqtnf#'
+    assert type(encrypt_message('Bugsnax')) == str
+
+    assert lucky_number("667766") == True
+    assert lucky_number("111111") == True
+    assert lucky_number("000000") == True
+    assert lucky_number("123456") == False
+    assert type(lucky_number("123456")) == bool
+
+    assert fizz_buzz(15) == "FizzBuzz"
+    assert fizz_buzz(30) == "FizzBuzz"
+    assert fizz_buzz(18) == "Fizz"
+    assert fizz_buzz(25) == "Buzz"
+    assert fizz_buzz(13) == "13"
+
+    assert password_is_strong("12345678Da") == True
+    assert password_is_strong("1234567890") == False
+    assert password_is_strong("12345Da") == False
+    assert password_is_strong("&&&&&&&&&&") == False
+    assert password_is_strong("yfugwuy32674896>V>") == False
+
+    assert number_is_prime(2) == True
+    assert number_is_prime(1) == False
+    assert number_is_prime(45) == False
+    assert number_is_prime(67) == True
+    assert number_is_prime(75) == False
+
+    assert decrypt_message('Fkoc') == 'Dima'
+    assert decrypt_message('Mquvkc') == 'Kostia'
+    assert decrypt_message('Dwiupcz') == 'Bugsnax'
+    assert decrypt_message('Jgnnq"Yqtnf#') == 'Hello World!'
+    assert type(decrypt_message('WDiuhf')) == str
+
+    assert type(prs("paper")) == bool
+    assert type(prs("scissors")) == bool
+    assert type(prs("rock")) == bool
+
+    assert volume_of_sphere(10.0) == 4188.79
+    assert volume_of_sphere(10.0) != 4188.7902
+    assert type(volume_of_sphere(10.0)) == float
+    assert volume_of_sphere(5.5) == 696.91
+    assert volume_of_sphere(20.0) == 33510.32
+
+    assert days_diff("2021-1-5", "2021-1-1") == 4
+    assert days_diff("2021-01-05", "2021-01-01") == 4
+    assert days_diff("2021-1-5", "2020-1-5") == 366
+    assert days_diff("2020-1-5", "2021-1-5") == 366
+    assert days_diff("2021-1-10", "2021-1-10") == 0
+
     assert integer_as_roman(1) == 'I'
     assert integer_as_roman(2) == 'II'
     assert integer_as_roman(3) == 'III'
